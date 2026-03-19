cce847c993337d21f5cc36d4a86efafb46624eeb
admin <user@user-OptiPlex-5000>
2026-03-19 14:24:58 +0530

    AOS semaphore

--- SEM 2/AOS/peterson.c
+++ SEM 2/AOS/peterson.c
@@ -1,46 +1,73 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
 int flag[2] = {0,0};
 int turn;
+int count = 0;
 
-void *p0()
+void *process0()
 {
-    flag[0] = 1;
+    flag[0] = 1;      // interested
     turn = 1;
-    while(flag[1] && turn == 1);
 
-    printf("Process 0 in CS \n");
+    while(flag[1] && turn == 1);   // wait
 
-    flag[0] = 0;
+    // CRITICAL SECTION
+    printf("Process 0 entered CS\n");
+
+    int temp = count;
+    sleep(1);
+    temp++;
+    count = temp;
+
+    printf("Process 0 updated count = %d\n", count);
+
+    flag[0] = 0;      // exit
+
+    return NULL;
 }
 
-void *p1()
+void *process1()
 {
     flag[1] = 1;
     turn = 0;
+
     while(flag[0] && turn == 0);
 
-    printf("Process 1 in CS\n");
+    // CRITICAL SECTION
+    printf("Process 1 entered CS\n");
+
+    int temp = count;
+    sleep(1);
+    temp++;
+    count = temp;
+
+    printf("Process 1 updated count = %d\n", count);
 
     flag[1] = 0;
+
+    return NULL;
 }
 
 int main()
 {
     pthread_t t1, t2;
 
-    pthread_create(&t1,NULL,p0,NULL);
-    pthread_create(&t2,NULL,p1,NULL);
+    pthread_create(&t1,NULL,process0,NULL);
+    pthread_create(&t2,NULL,process1,NULL);
 
     pthread_join(t1,NULL);
     pthread_join(t2,NULL);
 
+    printf("Final count = %d\n", count);
+
     return 0;
 }
 /*
-> gcc peterson.c -o output -lpthread
-> ./output
-Process 0 in CS
-Process 1 in CS
+Process 0 entered CS
+Process 0 updated count = 1
+Process 1 entered CS
+Process 1 updated count = 2
+Final count = 2
 */
--- SEM 2/AOS/semaphore.c
+++ SEM 2/AOS/semaphore.c
@@ -1,37 +1,54 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 sem_t s;
+int count = 0;
 
-void *fun()
+void *fun(void *arg)
 {
-    sem_wait(&s);   // enter
+    int id = *(int*)arg;
 
-    printf("Inside CS\n");
+    sem_wait(&s);   // ENTRY SECTION (lock)
 
-    sem_post(&s);   // exit
+    printf("Thread %d entered CS\n", id);
+
+    int temp = count;      // read shared data
+    sleep(1);              // simulate delay
+    temp++;
+    count = temp;          // update shared data
+
+    printf("Thread %d updated count = %d\n", id, count);
+
+    sem_post(&s);   // EXIT SECTION (unlock)
+
+    return NULL;
 }
 
 int main()
 {
-    pthread_t t1,t2;
+    pthread_t t1, t2;
+    int id1 = 1, id2 = 2;
 
-    sem_init(&s,0,1);
+    sem_init(&s, 0, 1);
 
-    pthread_create(&t1,NULL,fun,NULL);
-    pthread_create(&t2,NULL,fun,NULL);
+    pthread_create(&t1, NULL, fun, &id1);
+    pthread_create(&t2, NULL, fun, &id2);
 
-    pthread_join(t1,NULL);
-    pthread_join(t2,NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Final count = %d\n", count);
 
     sem_destroy(&s);
 
     return 0;
 }
 /*
-> gcc semaphore.c -o output -lpthread
-> ./output
-Inside CS
-Inside CS
+Thread 1 entered CS
+Thread 1 updated count = 1
+Thread 2 entered CS
+Thread 2 updated count = 2
+Final count = 2
 */
