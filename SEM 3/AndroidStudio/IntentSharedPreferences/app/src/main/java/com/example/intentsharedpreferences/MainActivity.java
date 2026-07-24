156304160b6da02983f7d377f796c55d4af69fb4
AnnEldho <annmaryeldho23@gmail.com>
2026-07-24 12:05:05 +0530

    Improved IntentSharedPreferences Android Studio project

--- SEM 3/AndroidStudio/IntentSharedPreferences/app/src/main/java/com/example/intentsharedpreferences/MainActivity.java
+++ SEM 3/AndroidStudio/IntentSharedPreferences/app/src/main/java/com/example/intentsharedpreferences/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.intentsharedpreferences;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,45 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText name,email;
+    Button submit;
+    SharedPreferences sp;
+    String n,e;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        name=(EditText) findViewById(R.id.name);
+        email=(EditText) findViewById(R.id.email);
+        submit=(Button) findViewById(R.id.submit);
+
+        submit.setOnClickListener(this);
+
+        sp = getSharedPreferences("MyData", MODE_PRIVATE);
+
+        if(sp.contains("Name"))
+            name.setText(sp.getString("Name",""));
+        if(sp.contains("Email"))
+            name.setText(sp.getString("Email",""));
+
+    }
+
+    @Override
+    public void onClick(View view) {
+       n=name.getText().toString();
+       e=email.getText().toString();
+
+       SharedPreferences.Editor editor=sp.edit();
+        editor.putString("Name", n);
+        editor.putString("Email", e);
+        editor.apply();
+
+        Intent i = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(i);
     }
 }
